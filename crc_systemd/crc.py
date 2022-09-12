3b3d32be14ed9b35f399b0ff2cdc03281676d4fc
Kevin M Granger <git@kevinmgranger.me>
2022-09-12 15:30:53 -0400

    Handle case where vm hasn't been run for the first time

--- crc_systemd/crc.py
+++ crc_systemd/crc.py
@@ -31,26 +31,39 @@ class OpenShiftStatus(enum.Enum):
     degraded = "Degraded"
 
 
+class NotYetExtant:
+    MESSAGE = "Machine does not exist. Use 'crc start' to create it"
+    ready = False
+
+    def __init__(self, timestamp: datetime.datetime):
+        self.timestamp = timestamp
+
+    @staticmethod
+    def __str__():
+        return NotYetExtant.MESSAGE
+
+
 class StatusOutput:
-    success: bool
-    crc_status: CrcStatus
-    openshift_status: OpenShiftStatus
-    timestamp: datetime.datetime
-
-    __match_args__ = ("success", "crc_status", "openshift_status")
-
-    def __init__(self, success: bool, crcStatus: str, openshiftStatus: str, timestamp: datetime.datetime, **_kwargs):
-        self.success = success
+    def __init__(
+        self,
+        crcStatus: str,
+        openshiftStatus: str,
+        timestamp: datetime.datetime,
+        **_kwargs,
+    ):
         self.crc_status = CrcStatus(crcStatus)
         self.openshift_status = OpenShiftStatus(openshiftStatus)
         self.timestamp = timestamp
 
     def __str__(self) -> str:
-        return f"success={self.success} crc={self.crc_status.value} openshift={self.openshift_status.value}"
+        return f"crc={self.crc_status.value} openshift={self.openshift_status.value}"
 
     @property
     def ready(self) -> bool:
-        return (self.crc_status, self.openshift_status) == (CrcStatus.running, OpenShiftStatus.running)
+        return (self.crc_status, self.openshift_status) == (
+            CrcStatus.running,
+            OpenShiftStatus.running,
+        )
 
     @property
     def notify_status(self) -> str:
@@ -58,13 +71,9 @@ class StatusOutput:
 
 
 class CrcMonitor:
-    # interval: float
-    # last_status: StatusOutput | None
-    # monitor_task: asyncio.Task[None]
-
     def __init__(self, interval: float):
         self.interval = interval
-        self.last_status : StatusOutput | None = None
+        self.last_status: StatusOutput | NotYetExtant | None = None
         self.monitor_task = asyncio.create_task(self._monitor())
         self.ready = asyncio.Event()
 
@@ -78,23 +87,28 @@ class CrcMonitor:
             self.last_status = await status()
             print(datetime.datetime.now(), self.last_status)
             if self.last_status.ready:
-                self.ready.set() # go
+                self.ready.set()  # go
             await asyncio.sleep(self.interval)
 
-async def monitor_generator() -> AsyncIterator[StatusOutput]:
+
+async def monitor_generator() -> AsyncIterator[StatusOutput | NotYetExtant]:
     while True:
         yield await status()
+
 
 async def start() -> asyncio.subprocess.Process:
     return await asyncio.create_subprocess_exec(CRC_PATH, *CRC_START_ARGS)
 
 
-async def status() -> StatusOutput:
+async def status() -> StatusOutput | NotYetExtant:
     status_proc = await asyncio.create_subprocess_exec(
         CRC_PATH, *CRC_STATUS_ARGS, stdout=asyncio.subprocess.PIPE
     )
     stdout_bytes = await status_proc.stdout.read()  # type: ignore
-    return StatusOutput(**json.loads(stdout_bytes), timestamp=datetime.datetime.now())
+    output = json.loads(stdout_bytes)
+    if not output["success"] and output["error"] == NotYetExtant.MESSAGE:
+        return NotYetExtant(datetime.datetime.now())
+    return StatusOutput(**output, timestamp=datetime.datetime.now())
 
 
 async def stop() -> asyncio.subprocess.Process:
