eca9d747228084c3c5cf1ae9cb575530a0b34b32
Kevin M Granger <git@kevinmgranger.me>
2022-10-07 22:02:25 -0400

    make signal helper work, maybe

--- crc_systemd/async_helpers.py
+++ crc_systemd/async_helpers.py
@@ -1,52 +1,83 @@
 import asyncio
+from collections import UserDict
 import collections.abc as abc
 import signal
-import typing
-from asyncio.subprocess import Process
-
-sigterm = asyncio.Event()
+from typing import TypeVar, Any
 
 
-def handle_sigterm():
-    sigterm.set()
+
+class _SignalListenerMap(UserDict[int, asyncio.Event]):
+    def handle(self):
+        for event in self.data.values():
+            event.set()
+            # TODO: it says set _immediately_ wakes them up, so is immediate clearing okay?
+            event.clear()
+
+    def __getitem__(self, id: int) -> asyncio.Event:
+        if id not in self.data:
+            self.data[id] = event = asyncio.Event()
+            return event
+        else:
+            return self.data[id]
+
+
+class _SignalMap(UserDict[signal.Signals, _SignalListenerMap]):
+    def listener_map_for(self, signal: signal.Signals) -> _SignalListenerMap:
+        "Idempotently register the handler for the given signal. Returns the map."
+        if signal in self:
+            return self[signal]
+        task_mapper = _SignalListenerMap()
+        self[signal] = task_mapper
+
+        loop = asyncio.get_running_loop()
+        loop.add_signal_handler(signal, task_mapper.handle)
+
+        return task_mapper
+
+
+_SIGNAL_MAP = _SignalMap()
 
 
 class SignalError(Exception):
-    def __init__(self, *args: object, signal: signal.Signals):
-        super().__init__(signal.name, *args)
+    __match_args__ = ("signal",)
+
+    def __init__(self, signal: signal.Signals):
+        super().__init__(signal)
         self.signal = signal
 
 
-class SigTermError(SignalError):
-    def __init__(self, *args: object):
-        super().__init__(*args, signal=signal.SIGTERM)
-
-
-T = typing.TypeVar("T")
+T = TypeVar("T")
 
 
 async def check_signal(
-    awaitable: abc.Coroutine[typing.Any, typing.Any, T] | asyncio.Task[T]
+    awaitable: abc.Coroutine[Any, Any, T] | asyncio.Task[T],
+    signal: signal.Signals,
 ) -> T:
     """
     Run a coroutine, throwing an exception if
-    a SIGTERM is recieved while doing so.
+    a signal is recieved while doing so.
     """
-    queue_task = asyncio.create_task(sigterm.wait())
+
     match awaitable:
         case asyncio.Task():
             awaitable_task = awaitable
         case _ if asyncio.iscoroutine(awaitable):
             # TODO why doesn't the type propogate?
+            # answer: because iscoroutine is a type guard that doesn't have an overload
             awaitable_task: asyncio.Task[T] = asyncio.create_task(awaitable)
         case _:
             raise TypeError("awaitable must be a coroutine or task")
 
-    while True:
-        done, _pending = await asyncio.wait(
-            (queue_task, awaitable_task), return_when=asyncio.FIRST_COMPLETED
-        )
-        if queue_task in done:
-            raise SigTermError
-        elif awaitable_task in done:
-            return awaitable_task.result()
+    signal_listener_map = _SIGNAL_MAP.listener_map_for(signal)
+    event = signal_listener_map[id(awaitable_task)]
+    event_task = asyncio.create_task(event.wait())
+
+    done, _pending = await asyncio.wait(
+        (event_task, awaitable_task), return_when=asyncio.FIRST_COMPLETED
+    )
+    if event_task in done:
+        raise SignalError(signal=signal)
+    elif awaitable_task in done:
+        return awaitable_task.result()
+    else:
+        raise RuntimeError("asyncio.wait returned even though no tasks were done")
